1f4c5660974adcbf28aaccf12772f5ee5a724554
Gudjon <gudjonss12@ru.is>
2016-10-11 13:12:25 +0000

    class folder

--- src/test/StringCalculatorTest.java
+++ src/test/StringCalculatorTest.java
@@ -1,1 +1,18 @@
-// Todo implement unit tests
+
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;// Todo implement unit tests
+
+public class StringCalculatorTest {
+
+	@Test
+	public void testEmptyString() {
+		assertEquals(0, StringCalculator.Add(""));
+	}
+
+
+
+
+
+
+}
